ce497bd7af21cefb86ace3945a6c811d490fb991
Badhon7821 <bayozidbostamibadhon@gmail.com>
2023-02-20 02:30:14 -0800

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,11 @@
+import moment from "moment";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-slate-800 py-8 text-center">
+      Proxima. All rights reserve. {moment().format("MMM DD YYYY")}
+    </div>
+  );
 };
 
 export default Footer;
