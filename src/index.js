3d9e13b5f9f318663c3455d3f3b8950016912182
Badhon7821 <bayozidbostamibadhon@gmail.com>
2023-03-03 01:47:58 -0800

    login and logout reducer added

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import { ProjectContextProvider } from "./context/ProjectContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ProjectContextProvider>
-        <App />
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
       </ProjectContextProvider>
     </BrowserRouter>
   </React.StrictMode>
