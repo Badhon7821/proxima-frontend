3ffbaba858a90b278e5a850eced39fa758c58f80
Badhon7821 <bayozidbostamibadhon@gmail.com>
2023-03-03 01:56:14 -0800

    login and signup form UI done

---
+++ src/pages/Login.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {};
+
+  return (
+    <form
+      onSubmit={handleLogin}
+      className="login-form flex flex-col gap-5 py-20 mx-auto max-w-sm"
+    >
+      <h2 className="text-4xl font-medium text-sky-400 mb-10">Login</h2>
+
+      <div className="form-control flex flex-col gap-2">
+        <label
+          htmlFor="email"
+          className="cursor-pointer hover:text-sky-400 duration-300"
+        >
+          Email address
+        </label>
+        <input
+          type="email"
+          id="email"
+          placeholder="hello@react.dev"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="bg-transparent border border-slate-500 py-3 px-5 rounded-xl outline-none focus:border-sky-400 duration-300"
+        />
+      </div>
+
+      <div className="form-control flex flex-col gap-2">
+        <label
+          htmlFor="password"
+          className="cursor-pointer hover:text-sky-400 duration-300"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="bg-transparent border border-slate-500 py-3 px-5 rounded-xl outline-none focus:border-sky-400 duration-300"
+        />
+      </div>
+
+      <button
+        disabled={loading}
+        type="submit"
+        className="bg-sky-400 text-slate-900 py-3 rounded-xl hover:bg-sky-500 duration-300 mt-3"
+      >
+        Log in
+      </button>
+
+      {error && (
+        <p className="bg-rose-500/20 rounded-lg p-5 text-rose-500 border border-rose-500">
+          {error}
+        </p>
+      )}
+    </form>
+  );
+};
+
+export default Login;
---
+++ src/pages/Signup.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignup = () => {
+    e.preventDefault();
+  };
+
+  return (
+    <form
+      onSubmit={handleSignup}
+      className="signup-form flex flex-col gap-5 py-20 mx-auto max-w-sm"
+    >
+      <h2 className="text-4xl font-medium text-sky-400 mb-10">Signup</h2>
+
+      <div className="form-control flex flex-col gap-2">
+        <label
+          htmlFor="email"
+          className="cursor-pointer hover:text-sky-400 duration-300"
+        >
+          Email address
+        </label>
+        <input
+          type="email"
+          id="email"
+          placeholder="hello@react.dev"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="bg-transparent border border-slate-500 py-3 px-5 rounded-xl outline-none focus:border-sky-400 duration-300"
+        />
+      </div>
+
+      <div className="form-control flex flex-col gap-2">
+        <label
+          htmlFor="password"
+          className="cursor-pointer hover:text-sky-400 duration-300"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="bg-transparent border border-slate-500 py-3 px-5 rounded-xl outline-none focus:border-sky-400 duration-300"
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="bg-sky-400 text-slate-900 py-3 rounded-xl hover:bg-sky-500 duration-300 mt-3"
+      >
+        Sign up
+      </button>
+
+      <p className="bg-rose-500/20 rounded-lg p-5 text-rose-500 border border-rose-500">
+        error
+      </p>
+    </form>
+  );
+};
+
+export default Signup;
